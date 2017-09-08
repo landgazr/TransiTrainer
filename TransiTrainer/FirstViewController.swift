0e423a6fe38893ee83b833e8d29b840ab45e25c7
Gabe Stone <Gabe.Stone@zimmerbiomet.com>
2017-09-08 15:08:10 -0700

    Testing SimpleKML includes.

--- TransiTrainer/FirstViewController.swift
+++ TransiTrainer/FirstViewController.swift
@@ -8,16 +8,20 @@
 
 import UIKit
 
+
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that nevertheless can be recreated.
+        
     }
     
 
