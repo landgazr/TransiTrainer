e6d89cc3909fd58823bdb9a8ce5e53e4555dd7fd
Gabe Stone <Gabe.Stone@zimmerbiomet.com>
2017-09-08 17:24:34 -0700

    Added more SimpleKML classes to bridge.

--- libSimple-KML.h
+++ libSimple-KML.h
@@ -13,3 +13,10 @@
 #endif /* libSimple_KML_h */
 
 #include "Simple-KML/source/SimpleKML.h"
+#include "Simple-KML/source/SimpleKMLContainer.h"
+#include "Simple-KML/source/SimpleKMLDocument.h"
+#include "Simple-KML/source/SimpleKMLFeature.h"
+#include "Simple-KML/source/SimpleKMLPlacemark.h"
+#include "Simple-KML/source/SimpleKMLPoint.h"
+#include "Simple-KML/source/SimpleKMLPolygon.h"
+#include "Simple-KML/source/SimpleKMLLinearRing.h"
