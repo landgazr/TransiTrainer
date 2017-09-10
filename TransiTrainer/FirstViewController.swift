e2a81db64e4b48ca5474ae6c38c1976ef84578c4
Gabe Stone <Gabe.Stone@zimmerbiomet.com>
2017-09-09 21:43:46 -0700

    Successful test of KMLParser.

--- TransiTrainer/FirstViewController.swift
+++ TransiTrainer/FirstViewController.swift
@@ -23,30 +23,18 @@ class FirstViewController: UIViewController {
         
     }
     
-    func getKML(fp: String) -> SimpleKML {
-       
-        let u: URL = URL.init(fileURLWithPath: fp)
-       
-       
-        do {
-            
-            return SimpleKML.init(contentsOf: u)
-            NSLog("OK")
-            
-        } catch {
-            NSLog("error")
-        }
-
-        return SimpleKML.init()
-    }
     
-   
     @IBAction func actionAlert(_ sender: Any) {
         let myAlert = UIAlertController(title: "Alert", message: "This is an alert.", preferredStyle: .alert);
         myAlert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.cancel, handler: nil));
         show(myAlert, sender: self)
         let filepath = Bundle.main.path(forResource: "tm_rail_stops", ofType: "kml")
-        var myKML: SimpleKML = getKML(fp: filepath!)
+        let u: URL = URL.init(fileURLWithPath: filepath!)
+        var kp: KMLParser = KMLParser(url: u)
+        kp.parseKML()
+        for mka in kp.points {
+            NSLog(mka.coordinate.latitude.description + " " + mka.coordinate.longitude.description)
+        }
         NSLog("done")
     }}
 
